2c1da09f147b4e1fc489e023b4ede34e775523c0
myusername <jaatsaab735@gmail.com>
2023-11-08 18:38:58 +0530

    final commit

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
 paytmParams['ORDER_ID'] = uuid();
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
 paytmParams['TXN_AMOUNT'] = '100';
-// paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback';
+paytmParams['CALLBACK_URL'] = 'callback';
 paytmParams['EMAIL'] = 'gauravrider315@gmail.com';
 paytmParams['MOBILE_NO'] = '1234567852';
